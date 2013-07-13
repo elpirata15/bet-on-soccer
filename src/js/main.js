fa2a9698e2a4c6fd1d685e84d39364e3b6e83be4
Thomas Gläßle <t_glaessle@gmx.de>
2013-07-13 06:50:27 +0200

    Use require.js (2.1.8) to load javascript modules

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+// File:    main.js
+// Purpose: application entry point
+
+(function (scope) {
+  'use strict';
+
+  scope.require.config({
+    baseUrl:    'js',
+    paths: {
+      require:  'lib/require'
+    }
+  });
+}(this));
